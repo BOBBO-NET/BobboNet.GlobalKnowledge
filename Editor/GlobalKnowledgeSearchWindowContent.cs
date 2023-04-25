b2b00287816f4794bf598f3d0f2cfdcada5a6052
LAPTOP-3BPAUQ9G\Iseei <iseeicyhawkston@gmail.com>
2023-04-25 01:34:50 -0500

    Initial Commit

---
+++ Editor/GlobalKnowledgeSearchWindowContent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace BobboNet
+{
+    public class GlobalKnowledgeSearchWindowContent : SearchWindowContent
+    {
+        public GlobalKnowledgeSearchWindowContent(string initialString, UnityAction<string> onCompleteString, int maxKeys = 10) : base(initialString, onCompleteString, maxKeys)
+        {
+
+        }
+
+        protected override string[] GetKeys()
+        {
+            return GlobalKnowledgeKeyDatabase.GetKeys();
+        }
+    }
+}
